a290658af40b8c03722132b0c2573b2672f66b8b
Johnatas Montezuma <jmo@logisticsmaven.com>
2017-10-07 08:02:15 +0900

    saving added to original 2-3.R

--- Listings/Chapter 2/2-3.R
+++ Listings/Chapter 2/2-3.R
@@ -1,3 +1,7 @@
 # Up-to-date at time of writing (May 2016)
 url <- "http://trading.chrisconlan.com/SPstocks.csv"
 S <- as.character(read.csv(url, header = FALSE)[,1])
+
+# save S.R with stock list from URL
+setwd(rootdir)
+dump(list = "S", "S.R")
