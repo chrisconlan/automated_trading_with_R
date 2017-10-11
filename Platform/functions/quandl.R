f3c662b24b5e336ac94fada06f4bd206bf7ea0c7
Johnatas Montezuma <jmo@logisticsmaven.com>
2017-10-11 20:55:56 +0900

    quandl formula updated with start date,  quandl-run.R with example

--- Platform/functions/quandl.R
+++ Platform/functions/quandl.R
@@ -13,23 +13,24 @@
 # mydata = Quandl("FRED/GDP", start_date="2001-12-31", end_date="2005-12-31")
 # mydata_columns <- Quandl(c("WIKI/AAPL.8", "WIKI/AAPL.9"), start_date="2017-01-01")
 
-#qundl API  
+#qundl API
 quandl_api = "MYAPIKEY"
 
 #add my key
 Quandl.api_key(quandl_api)
 
-start_date <- "2017-01-01"
-
-quandl_get <- function(sym) {
+quandl_get <-
+function(sym, start_date = "2017-01-01") {
     require(devtools)
     require(Quandl)
     # create a vector with all lines
     tryCatch(Quandl(c(
-        paste0("WIKI/", sym, ".8"),
-        paste0("WIKI/", sym, ".9"),
-        paste0("WIKI/", sym, ".10"),
-        paste0("WIKI/", sym, ".11"),
-        paste0("WIKI/", sym, ".12"))
-    ))
+        paste0("WIKI/", sym, ".8"),  #  Adj. Open
+        paste0("WIKI/", sym, ".9"),  # Adj. High
+        paste0("WIKI/", sym, ".10"), # Adj. Low
+        paste0("WIKI/", sym, ".11"), # Adj. Close
+        paste0("WIKI/", sym, ".12")), # Adj. Volume
+        start_date = start_date,
+        type = "zoo"
+        ))
 }
